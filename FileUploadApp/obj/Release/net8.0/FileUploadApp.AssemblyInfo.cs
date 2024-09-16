e9b5eb5f6e7ea1989fa326edb3619ff441771b3a
Paulo <paulodiasred@gmail.com>
2024-09-16 18:39:57 -0300

    fix

--- FileUploadApp/obj/Release/net8.0/FileUploadApp.AssemblyInfo.cs
+++ FileUploadApp/obj/Release/net8.0/FileUploadApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileUploadApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4be67cd36a062ccf7eed5aed11256d8815fcc8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileUploadApp")]
